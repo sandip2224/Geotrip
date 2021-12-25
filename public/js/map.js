65c47a5706fba9935a458e40c869b51ad215e643
sandip2224 <sandipan2224@gmail.com>
2021-12-25 10:44:05 +0530

    💄 Updated map UI with Mapbox

---
+++ public/js/map.js
@@ -0,0 +1,54 @@
+mapboxgl.accessToken = 'pk.eyJ1Ijoic2FuZGlwMjIyNCIsImEiOiJja3hhNWhuY3EwZTZ6Mm9td3dvdnp0eThsIn0.K56FMglwPMghPY0Finou_g';
+var map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/streets-v11',
+    zoom: 3,
+    center: [-77.4144, 25.0759]
+})
+
+// Fetch locations from API
+async function getLocations() {
+    const res = await fetch('/api/v1/places')
+    const locs = await res.json()
+    const places = locs.data.map(loc => {
+        return {
+            'type': 'Feature',
+            'geometry': {
+                'type': 'Point',
+                'coordinates': [loc.location.coordinates[0], loc.location.coordinates[1]]
+            },
+            properties: {
+                placeId: loc.placeId,
+                icon: 'suitcase'
+            }
+        }
+    })
+    loadMapPoints(places)
+}
+
+function loadMapPoints(places) {
+    map.on('load', () => {
+        map.addSource('point', {
+            'type': 'geojson',
+            'data': {
+                'type': 'FeatureCollection',
+                'features': places
+            }
+        });
+        map.addLayer({
+            'id': 'points',
+            'type': 'symbol',
+            'source': 'point', // reference the data source
+            'layout': {
+                'icon-image': '{icon}-15',
+                'icon-size': 1.5,
+                'text-field': '{placeId}',
+                'text-font': ['Open Sans Semibold', 'Arial Unicode MS Bold'],
+                'text-offset': [0, 0.9],
+                'text-anchor': 'top'
+            }
+        })
+    })
+}
+
+getLocations()
